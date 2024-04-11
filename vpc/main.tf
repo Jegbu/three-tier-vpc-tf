1c7f78549578ba7f36a0b84c6f2b280663ae811e
Justin Egbufoama <jegbufoama@gmail.com>
2024-04-11 15:13:00 -0400

    NACL created for private subnet AZ 2 - WIP

--- vpc/main.tf
+++ vpc/main.tf
@@ -141,6 +141,34 @@ resource "aws_network_acl" "NACL_private_1" {
     Name = "Private NACL AZ 1"
   }
 }
+
+#NACL for Private Subnet AZ 2
+resource "aws_network_acl" "NACL_private_1" {
+  vpc_id = aws_vpc.jegbu_vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "10.0.4.0/24"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  tags = {
+    Name = "Private NACL AZ 2"
+  }
+}
+
 # Route Table creation
 resource "aws_route_table" "Jegbu" {
   vpc_id = aws_vpc.jegbu_vpc.id
